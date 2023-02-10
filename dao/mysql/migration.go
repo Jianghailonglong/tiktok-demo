0b1e8436079900782494b1893620dace0624b3e8
Yan Keyu <77918648+Greenery-S@users.noreply.github.com>
2023-02-10 20:29:22 +0800

    Update migration.go

--- dao/mysql/migration.go
+++ dao/mysql/migration.go
@@ -1,16 +1,16 @@
 package mysql
 
-func migration() {
-	//自动迁移模式,
-	//如果没有表,则自动建表
-	db.Set("gorm:table_options", "charset=utf8mb4").
-		AutoMigrate(
-			&User{},
-			&ChatRecord{},
-			&WsChatRecord{},
-			&Relation{},
-			&Comment{},
-			&Favorite{},
-			&Relation{},
-			&Video{})
-}
+// func migration() {
+// 	//自动迁移模式,
+// 	//如果没有表,则自动建表
+// 	db.Set("gorm:table_options", "charset=utf8mb4").
+// 		AutoMigrate(
+// 			&User{},
+// 			&ChatRecord{},
+// 			&WsChatRecord{},
+// 			&Relation{},
+// 			&Comment{},
+// 			&Favorite{},
+// 			&Relation{},
+// 			&Video{})
+// }
